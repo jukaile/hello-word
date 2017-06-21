5a396bc6b0d9b02f652ef571929aef464e3fceee
jukaile <jukaile123@gmail.com>
2017-06-21 19:48:47 +0800

    This is the first GitHub program that name is Hello World!

---
+++ Hello World/Hello World/Hello World.cpp
@@ -0,0 +1,15 @@
+// Hello World.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << "Hello World!!" << std::endl;
+	}
+    return 0;
+}
+
