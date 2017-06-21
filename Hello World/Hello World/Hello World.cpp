018345a09e8980972ba6096f4deff52f0e0ec1d8
jukaile <jukaile123@gmail.com>
2017-06-21 20:00:49 +0800

    再来一次嗯，看看效果~

--- Hello World/Hello World/Hello World.cpp
+++ Hello World/Hello World/Hello World.cpp
@@ -3,12 +3,16 @@
 
 #include "stdafx.h"
 
-
 int main()
 {
 	for (int i = 0; i < 10; i++)
 	{
 		std::cout << "Hello World!!" << std::endl;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("Hello World!!!\n");
 	}
     return 0;
 }
