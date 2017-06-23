6bc8e353d5dab56ff0c51c79a7adeabcb75cd52d
Kaile Ju <jukaile123@gmail.com>
2017-06-24 00:53:35 +0800

    vs2017的HelloWorld，用以测试vs2017与GitHub;

---
+++ HelloWorldForVS2017/HelloWorldForVS2017/HelloWorldForVS2017.cpp
@@ -0,0 +1,20 @@
+// HelloWorldForVS2017.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	using std::cin;
+	using std::cout;
+	using std::endl;
+
+	int t = 10;
+	while (t--)
+	{
+		cout << "HelloWorld!" << endl;
+	}
+    return 0;
+}
+
